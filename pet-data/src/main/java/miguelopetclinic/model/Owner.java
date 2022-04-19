ad0d73e01c76503bf0c751ddd1193e35b86eda78
elmiguel0 <elmiguelo@live.com>
2022-04-18 22:59:21 -0500

    Create Pet, Type, Pet, Visit Entities, close #5

--- pet-data/src/main/java/miguelopetclinic/model/Owner.java
+++ pet-data/src/main/java/miguelopetclinic/model/Owner.java
@@ -1,4 +1,13 @@
 package miguelopetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pets;
+    public Set<Pet> getPets() {
+        return pets;
+    }
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
