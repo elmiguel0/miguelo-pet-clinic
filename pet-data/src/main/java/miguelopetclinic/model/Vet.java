7fce8a9e5662635bb3fccaba06bcc3d36ccdb111
elmiguel0 <elmiguelo@live.com>
2022-04-18 23:02:59 -0500

    Create Vet Speciality Entity, Associate to Vet. close #6

--- pet-data/src/main/java/miguelopetclinic/model/Vet.java
+++ pet-data/src/main/java/miguelopetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package miguelopetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    private Set<Specialty> specialties;
 }
