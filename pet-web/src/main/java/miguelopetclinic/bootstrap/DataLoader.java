5f44134999f9d22da5842324d2ffb376e658688d
elmiguel0 <miguelangel.ramos@ibm.com>
2022-05-04 22:22:43 -0500

    Create PetType on bootstrap loader. this close #11

--- pet-web/src/main/java/miguelopetclinic/bootstrap/DataLoader.java
+++ pet-web/src/main/java/miguelopetclinic/bootstrap/DataLoader.java
@@ -1,5 +1,7 @@
 package miguelopetclinic.bootstrap;
 
+import miguelopetclinic.model.PetType;
+import miguelopetclinic.service.PetTypeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,65 +15,56 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
-	
-	public DataLoader (OwnerService ownerService,VetService vetService){
+	private final PetTypeService petTypeService;
+	public DataLoader (OwnerService ownerService, VetService vetService, PetTypeService petTypeService){
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType saveDogPetType = petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		PetType saveCatPetType = petTypeService.save(cat);
+
 		Owner owner1 = new Owner();
-		
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
-		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-	
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glenanne");
-		
 		ownerService.save(owner2);
 		
 		Owner owner3 = new Owner();
-	
 		owner3.setFirstName("Miguel");
 		owner3.setLastName("Ramos");
-		
 		ownerService.save(owner3);
 		
 		System.out.println("Loaded Owners.....");
 		
 		Vet vet1 = new Vet();
-		
-	
 		vet1.setFirstName("Sam");
 		vet1.setLastName("Axe");
-		
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		
-	
 		vet2.setFirstName("Jessie");
 		vet2.setLastName("Porter");
-		
 		vetService.save(vet2);
 		
 		System.out.println("Loaded Vets.....");
 		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
+
+
 	
 	
 
